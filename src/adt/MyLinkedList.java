ff02c804243976dee0adbd590f921aa1ea00a2c8
yashsinghcs <yash.singh_cs18@gla.glc.in>
2020-01-23 11:03:52 +0530

    add getter methods

--- src/adt/MyLinkedList.java
+++ src/adt/MyLinkedList.java
@@ -5,6 +5,14 @@ public class MyLinkedList<E> implements LinkedListADT<E> {
         private E data;
         private Node<E> next;
 
+        public E getData() {
+            return data;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
     }
 
     @Override
